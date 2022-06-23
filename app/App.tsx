8f83c69b0420d91a8092a263d9b2fb3c220f7665
Paula Rolim <rolim.paula1999@gmail.com>
2022-06-22 23:03:54 -0300

    feat: apply light theme to app

--- app/App.tsx
+++ app/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { AuthProvider } from './src/contexts/authContext';
+import { AuthProvider } from "./src/contexts/authContext";
 
-import Routes from './src/routes/';
+import Routes from "./src/routes/";
+import { ThemeProvider, useTheme } from "styled-components/native";
+import { lightTheme } from "./src/global/theme";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
-    </NavigationContainer>
+    <ThemeProvider theme={lightTheme}>
+      <NavigationContainer>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
