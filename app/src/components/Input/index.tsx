7f2993540e803cdcc26d0b0da3bad27a5162bbf8
Paula Rolim <rolim.paula1999@gmail.com>
2022-06-22 23:06:09 -0300

    feat: add input component

---
+++ app/src/components/Input/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { StyledInput } from "./styles";
+import { InputProps } from "./types";
+
+export const Input = (props: InputProps) => <StyledInput {...props} />;
---
+++ app/src/components/Input/styles.ts
@@ -0,0 +1,10 @@
+import { TextInput } from "react-native";
+import styled from "styled-components/native";
+
+export const StyledInput = styled(TextInput)`
+  background-color: ${({ theme }) => theme.secondary};
+  border-radius: 15px;
+  padding: 10px 15px;
+  margin-bottom: 30px;
+  font-size: 16px;
+`;
